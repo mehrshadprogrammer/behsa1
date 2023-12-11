d439bcce1bc72fb2fd6ac59c1c4e2e2e5859995c
mehrshadprogrammer <mehrshadprogrammer@gmail.com>
2023-12-11 13:32:49 -0500

    task 1 and task 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+const App = () => {
+  const [names, setNames] = useState(['mehrshad', 'samaei']);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleRandomName = () => {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    alert(names[randomIndex]);
+  };
+
+  const handleAddName = () => {
+    setNames([...names, inputValue]);
+    setInputValue('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <button onClick={handleRandomName}>نمایش نام تصادفی</button>
+      <br />
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddName}>اضافه کردن نام</button>
+      <button onClick={() => alert(names.join(', '))}>دیدن لیست اسامی</button>
     </div>
   );
-}
-
+};
 export default App;
